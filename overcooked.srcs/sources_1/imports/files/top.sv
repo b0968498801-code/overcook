763462967d14c65a089f751dd0c35ebe50ef73bd
b0968498801-code <b0968498801@gmail.com>
2026-05-28 00:40:36 +0800

    add test comment in top module

--- overcooked.srcs/sources_1/imports/files/top.sv
+++ overcooked.srcs/sources_1/imports/files/top.sv
@@ -1,6 +1,6 @@
 // Top Level Module - Overcooked on Basys3
 // Connects all submodules
-
+//幹
 module top (
     input  logic        clk,            // 100MHz
     input  logic        btnC,           // start
